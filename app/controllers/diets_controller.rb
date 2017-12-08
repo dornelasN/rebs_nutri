f68d538a93696cce7ef3c809e42cd3991ba0ebb0
Nelson Dornelas <ndornelasj@gmail.com>
2017-12-08 10:22:51 -0800

    Problem trying to post a file towards another user instead of current_user

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -10,7 +10,13 @@ class DietsController < ApplicationController
   end
 
   def create
+    #TODO: the diet should be saved to the @user on the view and not to the current_user
+    # The following commented code is what I thought would work, but the :user_id or :id always return nil
+    # so I'm not able to retrieve the user that is showing in the view to post a new pdf file to his 'diet' attribute
+    # @user = User.find(params[:user_id]) # or User.find(user_params[:id])
+    # @diet = @user.diets.build(diet_params)
     @diet = current_user.diets.build(diet_params)
+    # whereas in this code, the pdf diet is posted to the current_user (admin) 
     if @diet.save
       flash[:success] = "Diet uploaded!"
       redirect_to root_url
